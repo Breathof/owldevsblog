11c65e0f03f756564744044b7d6f470a62e09503
Clovis <nachofcz@mail.com>
2020-01-23 23:50:10 -0300

    express implementation

--- functions/index.js
+++ functions/index.js
@@ -6,33 +6,27 @@ const admin = require('firebase-admin');
 const express =  require('express');
 const app = express();
 
-var serviceAccount = require("../../owldevs-sdk-key.json");
-
+var serviceAccount = require(".owldevs-sdk-key.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://owldevs-8ecbc.firebaseio.com"
 });
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+// admin.initializeApp();
 
-exports.getPosts = functions.https.onRequest((req, res) => {
+app.get('/posts', (req, res) =>{
     admin.firestore().collection('post').get()
-        .then((data) => {
-            let posts = [];
-            data.forEach((doc) => {
-                posts.push(doc.data());
-            });
-            return res.json(posts);
-        })
-        .catch(err => console.log(err));
-});
+    .then((data) => {
+        let posts = [];
+        data.forEach((doc) => {
+            posts.push(doc.data());
+        });
+        return res.json(posts);
+    })
+    .catch(err => console.log(err));
+})
 
-exports.createPost = functions.https.onRequest((req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(400).json({ error: 'Method not allawed' });
-    }
+app.post('post', (req, res) => {
     const newPost = {
         body: req.body.body,
         user: req.body.user,
@@ -51,3 +45,5 @@ exports.createPost = functions.https.onRequest((req, res) => {
             console.log(err);
         })
 });
+
+exports.api = functions.https.onRequest(app);
